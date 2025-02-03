8f751e5eaaf711380e80cca8bcf73062fe399171
Smit Chitroda <smitchitroda2210@gmail.com>
2025-02-03 18:09:29 +0530

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -6,6 +6,20 @@ class AuthServices {
 
   FirebaseAuth auth = FirebaseAuth.instance;
 
+   Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
   Future<void> signUpWithEmailAndPassword(
       {required email, required password}) async {
     try {
